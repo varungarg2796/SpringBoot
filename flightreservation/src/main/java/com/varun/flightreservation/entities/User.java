8f55860a8e4e5411e1ee87d8774eebd1b27f1ae5
varungarg2796 <varungarg2796@hotmail.com>
2020-08-25 23:04:08 +0100

    more spring

---
+++ flightreservation/src/main/java/com/varun/flightreservation/entities/User.java
@@ -0,0 +1,65 @@
+package com.varun.flightreservation.entities;
+
+
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+
+
+@Entity
+public class User extends AbstractEntity {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Set<Role> roles;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", password=" + password
+                + "]";
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+}
